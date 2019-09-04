af035eaf69bb3bb7946d9861d694d6c15addc1d2
Fiona Ye <fiona.ye@yandex.com>
2019-09-04 23:05:22 +0800

    update

--- bundle.js
+++ bundle.js
@@ -2,8 +2,6 @@
 const csjs = require('csjs-inject')
 const inputPayable = require('../')
 
-document.body.innerHTML = "<h1>Hello world</h1>"
-
 const classes = {
     inputContainer: 'inputContainer',
     inputParam: 'inputParam',
@@ -13,11 +11,83 @@ const classes = {
     currency: 'currency'
 }
 
-const log = document.createElement('pre')
-const el = inputPayable({ theme: { classes }, label})
+const colors = {
+    bodyFont: `'Nunito', sans-serif`,
+    inputParamColor: '#2c323c',
+    inputParamFontSize: '1.3rem',
+    inputParamTextAlign: 'right',
+    inputParamPadding: '6px 15px 0 0',
+    inputFieldTextAlign: 'center',
+    inputFieldColor: '#2c323c',
+    inputFieldPlaceholderColor: '#8d8d8d',
+    inputFieldBackgroundColor: 'transparent',
+    inputFieldBorder: '1px solid #ccc',
+    inputFieldBorderRadius: '2px',
+    inputFieldFontSize: '1.4rem',
+    ethIconFontSize: '1.6rem',
+    ethIconColor: '#3c3c3d',
+}
+
+document.body.innerHTML = `<style>
+@import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+@import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
+html {
+    font-size: 62.5%;
+}
+body {
+    font-size: 1.6rem;
+  }
+.inputContainer {
+    display: grid;
+    grid-template: auto / 100px auto;
+    margin-bottom: 15px;
+  }
+  .inputParam {
+    padding: ${colors.inputParamPadding};
+    color: ${colors.inputParamColor};
+    font-size: ${colors.inputParamFontSize};
+    text-align: ${colors.inputParamTextAlign};
+  }
+  .inputArea {
+    display: grid;
+    grid-template: auto / auto auto 30px;
+    grid-column-gap: 5px;
+  }
+  .inputField {
+    background-color: ${colors.inputFieldBackgroundColor};
+    border-radius: 2px;
+    color: ${colors.inputFieldColor};
+    border: ${colors.inputFieldBorder};
+    text-align: center;
+    font-family: ${colors.bodyFont};
+    font-size: ${colors.inputFieldFontSize};
+    padding: 5px;
+    width: calc(100% - 10px);
+  }
+  .inputField::placeholder {
+    color: ${colors.inputFieldPlaceholderColor};
+  }
+  .currency {
+    font-family: ${colors.bodyFont};
+    border-radius: ${colors.currencyBorderRadius};
+    border: ${colors.currencyBorder};
+    padding: 5px 7px;
+    color: ${colors.currencyColor};
+    background-color: ${colors.currencyBackgroundColor};
+    font-size: ${colors.currencyFontSize};
+  }
+  
+  .ethIcon {
+    color: ${colors.ethIconColor};
+    font-size: ${colors.ethIconFontSize};
+    text-align: center;
+    align-self: center;
+  }
+</style>`
+
+const el = inputPayable({ theme: { classes }, label: 'label'})
 
 document.body.appendChild(el)
-document.body.appendChild(log)
 },{"../":26,"csjs-inject":6}],2:[function(require,module,exports){
 var trailingNewlineRegex = /\n[\s]+$/
 var leadingNewlineRegex = /^\n[\s]+/
@@ -1097,7 +1167,7 @@ module.exports = inputPayable
 
 function inputPayable ({ theme: { classes: css }, label }) {
 
-  return bel`
+  const input = bel`
     <div class=${css.inputContainer}>
       <label class=${css.inputParam} title="data type: ${label}">value</label>
       <div class=${css.inputArea}>
@@ -1106,27 +1176,30 @@ function inputPayable ({ theme: { classes: css }, label }) {
         <div class=${css.ethIcon} title="Select amount you want to send with this function!"><i class="fab fa-ethereum"></i></div>
       </div>
     </div>`
-}
 
-function validate (e) {
+  return input
+  function validate (e) {
     // @TODO
+  }
+
+  function currencySelector (css) {
+    var button = bel`
+      <select class=${css.currency}>
+        <option value="wei">wei</option>
+        <option value="k-wei">k-wei</option>
+        <option value="m-wei">m-wei</option>
+        <option value="g-wei">g-wei</option>
+        <option value="micro">micro</option>
+        <option value="milli">milli</option>
+        <option value="ether">ether</option>
+        <option value="k-ether">k-ether</option>
+        <option value="m-ether">m-ether</option>
+        <option value="g-ether">g-ether</option>
+        <option value="t-ether">t-ether</option>
+      </select>`
+    return button
+  }
 }
 
-function currencySelector (css) {
-  var button = bel`
-    <select class=${css.currency}>
-      <option value="wei">wei</option>
-      <option value="k-wei">k-wei</option>
-      <option value="m-wei">m-wei</option>
-      <option value="g-wei">g-wei</option>
-      <option value="micro">micro</option>
-      <option value="milli">milli</option>
-      <option value="ether">ether</option>
-      <option value="k-ether">k-ether</option>
-      <option value="m-ether">m-ether</option>
-      <option value="g-ether">g-ether</option>
-      <option value="t-ether">t-ether</option>
-    </select>`
-  return button
-}
+
 },{"bel":3}]},{},[1]);
